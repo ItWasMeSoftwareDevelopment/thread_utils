1fec16f8db0d8a26362ca3eb1ab11a7f5354f008
Oleg Krymskii <okrymsky@gmail.com>
2020-05-09 19:19:20 +0400

    Create project

---
+++ ThreadUtils/DelayService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ItWasMe.ThreadUtils
+{
+    public interface IDelayService
+    {
+        void ThreadSleep(TimeSpan sleep);
+        void ThreadSleep(int ms);
+        Task DelayAsync(TimeSpan sleep);
+        Task DelayAsync(int ms);
+        Task DelayAsync(TimeSpan sleep, CancellationToken token);
+        Task DelayAsync(int ms, CancellationToken token);
+    }
+
+    public class DelayService : IDelayService
+    {
+        public void ThreadSleep(TimeSpan sleep)
+        {
+            Thread.Sleep(sleep);
+        }
+        public void ThreadSleep(int ms)
+        {
+            Thread.Sleep(ms);
+        }
+
+        public async Task DelayAsync(TimeSpan sleep)
+        {
+            await Task.Delay(sleep);
+        }
+
+        public async Task DelayAsync(int ms)
+        {
+            await Task.Delay(ms);
+        }
+
+        public async Task DelayAsync(TimeSpan sleep, CancellationToken token)
+        {
+            await Task.Delay(sleep, token);
+        }
+
+        public async Task DelayAsync(int ms, CancellationToken token)
+        {
+            await Task.Delay(ms, token);
+        }
+    }
+}
